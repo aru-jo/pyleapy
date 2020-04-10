ff4684deccbd20ed1e3e688b13bed8afa4c6f7a8
Aravind Jyothi <demievent@gmail.com>
2020-04-10 02:58:46 -0700

    created new version 0.0.2 and added py3 wheel

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup 
 setup(
 	name = "pyleap",
-	version = "0.0.1", 
+	version = "0.0.2", 
 	description = "A simple python module to check whether a year is Leap year or not",  
 	py_modules = ["pyleap"],  
 	package_dir = {"":"src"},
 	author = 'Aravind Jyothi',                 
         author_email = 'demievent@gmail.com',      
         url = 'https://github.com/aru-jo/pyleap',   
- 	download_url = 'https://github.com/aru-jo/pyleap/archive/0.0.1.tar.gz',    
+ 	download_url = 'https://github.com/aru-jo/pyleap/archive/0.0.2.tar.gz',    
   	keywords = ['LEAP', 'YEAR', 'CHECK']
 ) 
